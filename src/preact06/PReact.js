d912ed2a43e75812c51a6373ed66223e99fd4a06
zhengbeiyun <zhengbeiyun@gmail.com>
2024-06-05 13:55:10 +0000

    event binding

---
+++ src/preact06/PReact.js
@@ -0,0 +1,227 @@
+import '../requestIdleCallbackPolyfill'
+
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.flat().map(child => {
+                if (typeof child === 'object') {
+                    return child
+                }
+                return createTextElement(child)
+            }),
+        }
+    }
+}
+
+function createTextElement(text) {
+    return {
+        type: 'HostText',
+        props: {
+            nodeValue: text,
+            children: [],
+        }
+    }
+}
+
+let workInProgress = null
+let workInProgressRoot = null
+let currentHookFiber = null
+let currentHookIndex = 0
+const isEvent = key => key.startsWith('on')
+const isProperty = key => key !== 'children' && !isEvent(key)
+
+class RootElement {
+    _internalRoot = null
+    constructor(container) {
+        this._internalRoot = {
+            current: null,
+            container,
+        }
+    }
+    render(element) {
+        this._internalRoot.current = {
+            alternate: {
+                stateNode: this._internalRoot.container,
+                props: {
+                    children: [element]
+                }
+            }
+        }
+        workInProgressRoot = this._internalRoot
+        workInProgress = workInProgressRoot.current.alternate
+        window.requestIdleCallback(workloop)
+    }
+}
+
+function workloop() {
+    while (workInProgress) {
+        workInProgress = performUnitOfWork(workInProgress)
+    }
+    if (!workInProgress && workInProgressRoot.current.alternate) {
+        workInProgressRoot.current = workInProgressRoot.current.alternate
+        workInProgressRoot.current.alternate = null
+    }
+}
+
+function performUnitOfWork(fiber) {
+    console.log('perform ', fiber)
+    // 处理当前 fiber，创建 dom 设置属性
+    const isFunctionComponent = typeof fiber.type === 'function'
+    if (isFunctionComponent) {
+        currentHookFiber = fiber
+        currentHookFiber.memorizedState = []
+        currentHookIndex = 0
+        fiber.props.children = [fiber.type(fiber.props)]
+    } else {
+        if (!fiber.stateNode) {
+            fiber.stateNode = fiber.type === 'HostText' ? document.createTextNode('') : document.createElement(fiber.type)
+            Object.keys(fiber.props)
+                .filter(isProperty)
+                .forEach(key => {
+                    fiber.stateNode[key] = fiber.props[key]
+                })
+            Object.keys(fiber.props)
+                .filter(isEvent)
+                .forEach(key => {
+                    const eventName = key.toLowerCase().substring(2)
+                    fiber.stateNode.addEventListener(eventName, fiber.props[key])
+                })
+        }
+        if (fiber.return) {
+            let fiberParent = fiber.return
+            while (!fiberParent.stateNode) {
+                fiberParent = fiberParent.return
+            }
+            fiberParent.stateNode.appendChild(fiber.stateNode)
+        }
+    }
+    // 初始化 children 的 fiber
+    if (fiber.props.children) {
+        let prevSibling = null
+        // mount 阶段 oldFiber 为空，update 阶段为上一次的值
+        let oldFiber = fiber?.alternate?.child
+        fiber.props.children.forEach((child, index) => {
+            let newFiber = null
+            if (!oldFiber) {
+                // mount
+                newFiber = {
+                    type: child.type,
+                    stateNode: null,
+                    props: child.props,
+                    return: fiber,
+                    alternate: null,
+                    child: null,
+                    sibling: null,
+                }
+            } else {
+                // update
+                newFiber = {
+                    type: child.type,
+                    stateNode: oldFiber.stateNode,
+                    props: child.props,
+                    return: fiber,
+                    alternate: oldFiber,
+                    child: null,
+                    sibling: null,
+                }
+            }
+
+            if (oldFiber) {
+                oldFiber = oldFiber.sibling
+            }
+            if (index === 0) {
+                fiber.child = newFiber
+            } else {
+                prevSibling.sibling = newFiber
+            }
+            prevSibling = newFiber
+        })
+    }
+    // 返回下一个要处理的 fiber
+    return getNextFiber(fiber)
+}
+
+function getNextFiber(fiber) {
+    // 先遍历 child
+    if (fiber.child) {
+        return fiber.child
+    }
+    let nextFiber = fiber
+    while (nextFiber) {
+        // 再遍历 sibling
+        if (nextFiber.sibling) {
+            return nextFiber.sibling
+        }
+        // 最后是 return 并找下一个 sibling
+        nextFiber = nextFiber.return
+    }
+    return null
+}
+
+function createRoot(container) {
+    return new RootElement(container)
+}
+
+function act(callback) {
+    callback()
+    return new Promise(resolve => {
+        function loop() {
+            if (!workInProgress) {
+                resolve()
+            } else {
+                window.requestIdleCallback(loop)
+            }
+        }
+        loop()
+    })
+}
+
+function useState(initialState) {
+    const oldHook = currentHookFiber.alternate?.memorizedState?.[currentHookIndex]
+    const hook = {
+        // 承接上一次更新时的 state
+        state: oldHook ? oldHook.state : initialState,
+        queue: [],
+        dispatch: oldHook?.dispatch || null
+    }
+    const actions = oldHook ? oldHook.queue : []
+    actions.forEach(action => {
+        hook.state = action(hook.state)
+    })
+    const setState = hook.dispatch ? hook.dispatch : (action) => {
+        let _action = action
+        if (typeof action !== 'function') {
+            _action = () => action
+        }
+        hook.queue.push(_action)
+        // re-render
+        workInProgressRoot.current.alternate = {
+            stateNode: workInProgressRoot.current.container,
+            props: workInProgressRoot.current.props,
+            alternate: workInProgressRoot.current, // 重要，交换 alternate
+        }
+        workInProgress = workInProgressRoot.current.alternate
+        window.requestIdleCallback(workloop)
+    }
+    currentHookFiber.memorizedState.push(hook)
+    currentHookIndex++
+    return [hook.state, setState]
+}
+
+function useReducer(reducer, initialState) {
+    const [state, setState] = useState(initialState)
+    const dispatch = (action) => {
+        setState(state => reducer(state, action))
+    }
+    return [state, dispatch]
+}
+
+export default {
+    createElement,
+    createRoot,
+    act,
+    useState,
+    useReducer,
+}
---
+++ src/preact06/jsx.test.jsx
@@ -0,0 +1,117 @@
+import { describe, it, expect, vi, } from "vitest";
+import PReact from './PReact'
+
+describe('function Component', () => {
+    it('should render function Component with props', async () => {
+        function App(props) {
+            return (
+                <div id="test">
+                    <h1 className="welcome">{props.title}</h1>
+                    <button>add</button>
+                    {props.children}
+                </div>
+            )
+        }
+        const container = document.createElement('div')
+        const dom = (
+            <App title={'hello'}>
+                <span>inner</span>
+            </App>
+        )
+        const root = PReact.createRoot(container)
+        console.log('dom ', dom)
+        await PReact.act(() => {
+            root.render(dom)
+            expect(container.innerHTML).toEqual(``)
+        })
+        expect(container.innerHTML).toEqual(`<div id="test"><h1 class="welcome">hello</h1><button>add</button><span>inner</span></div>`)    
+    })
+
+    it('should support useState', async () => {
+        const globalObject = {}
+        function App(props) {
+            const [count, setCount] = PReact.useState(100)
+            globalObject.count = count
+            globalObject.setCount = setCount
+            return (
+                <div>
+                    {count}
+                </div>
+            )
+        }
+        const container = document.createElement('div')
+        const root = PReact.createRoot(container)
+        await PReact.act(() => {
+            root.render(<App />)
+        })
+        await PReact.act(() => {
+            globalObject.setCount(count => count + 1)
+        })
+        await PReact.act(() => {
+            globalObject.setCount(globalObject.count + 1)
+        })
+        expect(globalObject.count).toBe(102)
+    })
+
+    it('should support useReducer', async () => {
+        const globalObject = {}
+        function reducer(state, action) {
+            switch (action.type) {
+                case 'add':
+                    return state + 1
+                case 'sub':
+                    return state - 1
+            }
+        }
+        function App(props) {
+            const [count, dispatch] = PReact.useReducer(reducer, 100)
+            globalObject.count = count
+            globalObject.dispatch = dispatch
+            return (
+                <div>
+                    {count}
+                </div>
+            )
+        }
+        const container = document.createElement('div')
+        const root = PReact.createRoot(container)
+        await PReact.act(() => {
+            root.render(<App />)
+        })
+        await PReact.act(() => {
+            globalObject.dispatch({ type: 'sub' })
+            globalObject.dispatch({ type: 'sub' })
+        })
+        expect(globalObject.count).toBe(98)
+    })
+
+    it('should support eventBinding', async () => {
+        const globalObject = {
+            increase: (count) => count + 1
+        }
+        function App(props) {
+            const [count, setState] = PReact.useState(100)
+            globalObject.count = count
+            return (
+                <div>
+                    <button onClick={() => setState(globalObject.increase)}>
+                        {count}
+                    </button>
+                </div>
+            )
+        }
+        const increaseSpy = vi.spyOn(globalObject, 'increase')
+        const container = document.createElement('div')
+        const root = PReact.createRoot(container)
+        await PReact.act(() => {
+            root.render(<App />)
+        })
+        expect(increaseSpy).not.toBeCalled()
+        await PReact.act(() => {
+            container.querySelector('button').click()
+            container.querySelector('button').click()
+        })
+        expect(increaseSpy).toBeCalledTimes(2)
+        expect(globalObject.count).toBe(102)
+    })
+})
